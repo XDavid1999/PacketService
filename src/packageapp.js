2dfb9313b9086b7de1ebc10b28dc4145ac495eb5
David Heredia <xdavidheredia99@gmail.com>
2020-10-12 12:48:49 +0200

    Cambios en README y código

--- src/packageapp.js
+++ src/packageapp.js
@@ -1,14 +1,14 @@
 class Packet{
 
   /**
-     * Constructor básico de la clase Packet.
-     * @param {String} descripcion - Breve descripción del paquete
-     * @param {Number} peso - Peso del paquete
-     * @param {String} destino - Destino al que se enviará del paquete
-     * @param {String} origen - Fecha de nacimiento
-     * @param {String} localizacionActual - Localización actual del paquete
-     * @param {String} agencia - Agencia con la que se envirá el paquete 
-     */
+   * Constructor básico de la clase Packet.
+   * @param {String} descripcion - Breve descripción del paquete
+   * @param {Number} peso - Peso del paquete
+   * @param {String} destino - Destino al que se enviará del paquete
+   * @param {String} origen - Fecha de nacimiento
+   * @param {String} localizacionActual - Localización actual del paquete
+   * @param {String} agencia - Agencia con la que se envirá el paquete 
+   */
 
   constructor(descripcion, peso, destino, origen, agencia) {
     this.descripcion = descripcion;
@@ -58,24 +58,24 @@ class Packet{
     this.localizacionActual=localizacion;
   }
 
-  set descripcion(description){
-    this.descripcion=description;
+  set descripcion(nuevadescripcion){
+    this.descripcion=nuevadescripcion;
   }
 
-  set peso(weight){
-    this.peso=weight;
+  set peso(nuevopeso){
+    this.peso=nuevopeso;
   }
 
-  set destino(destiny){
-    this.destino=destiny;
+  set destino(nuevodestino){
+    this.destino=nuevodestino;
   }
 
-  set origen(origin){
-    this.origen=origin;
+  set origen(nuevoorigen){
+    this.origen=nuevoorigen;
   }
 
-  set agencia(agency){
-    this.agencia=agency;
+  set agencia(nuevaagencia){
+    this.agencia=nuevaagencia;
   }
 
   /**
