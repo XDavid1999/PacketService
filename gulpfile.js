9eae892af64226a8ece65122163e95a6d2877679
David Heredia <xdavidheredia99@gmail.com>
2020-10-15 20:45:56 +0200

    Actualizando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 mocha = require('gulp-mocha'),
 babel = require('babel-register');
+var exec = require('child_process').exec;
 
+/**Tarea para realizar test*/
 gulp.task('test', function() {
   return gulp.src(['test/*.js'])
     .pipe(mocha({
@@ -9,7 +11,17 @@ gulp.task('test', function() {
   }));
 });
 
+/**Tarea para instalar dependencias del proyecto*/
+gulp.task('install', function (cb) {
+  exec('npm install', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+/**Tarea por defecto, muestra las tareas posibles disponibles*/
 gulp.task('default', function(done) {
-  console.log('Available Tasks: [test]');
+  console.log('Available Tasks: [test, install]');
   done();
 });
