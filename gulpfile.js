c8a2b8783b36ef15197a33f7a45433b17f7e122c
David Heredia <xdavidheredia99@gmail.com>
2020-10-15 20:13:03 +0200

    Actualizando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp'),
-  mocha = require('gulp-mocha'),
-  babel = require('babel-register');
+mocha = require('gulp-mocha'),
+babel = require('babel-register');
+
 gulp.task('test', function() {
   return gulp.src(['test/*.js'])
     .pipe(mocha({
       compilers:babel
   }));
+});
+
+gulp.task('default', function(done) {
+  console.log('Available Tasks: [test]');
+  done();
 });
