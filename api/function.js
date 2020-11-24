1a9a83bbafecc9ac758af3ce5378ffa0ce19a5d6
David Heredia <xdavidheredia99@gmail.com>
2020-11-24 12:51:22 +0100

    Arreglo en vercel pra que devuelva JSON fin 2.0 #65 #66

--- api/function.js
+++ api/function.js
@@ -4,6 +4,7 @@ module.exports = (req, res) => {
     const { Nickusuario = 'invalid' } = req.query
     let mensaje = "";
     var paquetes = [];
+    var i=0;
 
     jsonData.forEach(function(obj) {
         if(obj.Nickusuario==Nickusuario)
@@ -11,16 +12,22 @@ module.exports = (req, res) => {
     });
 
     if(paquetes.length!==0){
+        mensaje="[";
         paquetes.forEach(function(obj) {
-            mensaje +="{" + "Propietario: " + obj.Nickusuario + " " +
-            "Descripción: " + obj.descripcion + " " +
-            "Peso: " + obj.peso + " " +
-            "Origen: " + obj.origen + " " +
-            "Destino: " + obj.destino + " " +
-            "Localización Actual: " + obj.localizacionActual + " " +
-            "Agencia de Transporte: " + obj.agencia + " " +
+            mensaje +="{" + "Propietario: " + obj.Nickusuario + " ," +
+            "Descripción: " + obj.descripcion + " ," +
+            "Peso: " + obj.peso + " ," +
+            "Origen: " + obj.origen + " ," +
+            "Destino: " + obj.destino + " ," +
+            "Localización Actual: " + obj.localizacionActual + " ," +
+            "Agencia de Transporte: " + obj.agencia + " ," +
             "Estado del envío: " + obj.estado + "}";
+
+            if(i<paquetes.length-1)
+                mensaje += ","
+            i++;
         });
+        mensaje+="]";
     }
     else
         mensaje="Parece que " + Nickusuario + " no tiene paquetes en curso en el sistema";
