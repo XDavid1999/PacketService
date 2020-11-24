314fd346463896fa478d37c24de4e764eca93710
David Heredia <xdavidheredia99@gmail.com>
2020-11-24 15:32:59 +0100

    Arreglo en vercel pra que devuelva JSON fixed #65 #66

--- api/function.js
+++ api/function.js
@@ -2,38 +2,18 @@ module.exports = (req, res) => {
 
     let jsonData = require('./datos.json');
     const { Nickusuario = 'invalid' } = req.query
-    let mensaje = "";
     var paquetes = [];
-    var i=0;
 
     jsonData.forEach(function(obj) {
         if(obj.Nickusuario==Nickusuario)
             paquetes.push(obj);
     });
 
-    // if(paquetes.length!==0){
-    //     mensaje="[";
-    //     paquetes.forEach(function(obj) {
-    //         mensaje +="{" + "Propietario: " + obj.Nickusuario + ", " +
-    //         "Descripción: " + obj.descripcion + ", " +
-    //         "Peso: " + obj.peso + ", " +
-    //         "Origen: " + obj.origen + ", " +
-    //         "Destino: " + obj.destino + ", " +
-    //         "Localización Actual: " + obj.localizacionActual + ", " +
-    //         "Agencia de Transporte: " + obj.agencia + ", " +
-    //         "Estado del envío: " + obj.estado + "}";
+    if(paquetes.length!=0)
+        res.status(200).json(paquetes);
+    else    
+        res.status(200).json("Parece que " + Nickusuario + " no tiene paquetes en curso en el sistema");
 
-    //         if(i<paquetes.length-1)
-    //             mensaje += ", "
-    //         i++;
-    //     });
-    //     mensaje+="]";
-    // }
-    // else
-    //     mensaje="Parece que " + Nickusuario + " no tiene paquetes en curso en el sistema";
-
-    res.status(200).json(paquetes);
-    
 }
 
 
