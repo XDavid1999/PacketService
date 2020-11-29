e46391c0f6e4587b38cce9889ea5ba75f8bcfe24
David Heredia <xdavidheredia99@gmail.com>
2020-11-29 20:25:00 +0100

    Mejorando función en vercel 19.0 #62 #66 #67 [skip CI]

--- api/function.js
+++ api/function.js
@@ -11,31 +11,31 @@ module.exports = (req, res) => {
                 paquetes.push(obj);
         });
 
-    if(req.query.estado!=undefined && paquetes.length!=0)
-        estado=req.query.estado;
-        if(EstadoPaquete.includes(estado)){
-            for (let i = 0; i < paquetes.length; i++) {
-                if(paquetes[i]['estado']!=estado)
-                    paquetes.splice(i, 1);
-            }
+    // if(req.query.estado!=undefined && paquetes.length!=0)
+    //     estado=req.query.estado;
+    //     if(EstadoPaquete.includes(estado)){
+    //         for (let i = 0; i < paquetes.length; i++) {
+    //             if(paquetes[i]['estado']!=estado)
+    //                 paquetes.splice(i, 1);
+    //         }
 
-            if(paquetes.length==0)
-                mensaje = "No hay paquetes con el estado especificado."
-        }
-        else{
-            mensaje = "El estado que ha indicado no está dentro de los posibles."
-            paquetes = [];        
-        }
+    //         if(paquetes.length==0)
+    //             mensaje = "No hay paquetes con el estado especificado."
+    //     }
+    //     else{
+    //         mensaje = "El estado que ha indicado no está dentro de los posibles."
+    //         paquetes = [];        
+    //     }
 
-    if(req.query.agencia!=undefined && paquetes.length!=0){
-        for (let i = 0; i < paquetes.length; i++) {
-            if(paquetes[i].agencia!=req.query.agencia)
-                paquetes.splice(i, 1);
-        }            
+    // if(req.query.agencia!=undefined && paquetes.length!=0){
+    //     for (let i = 0; i < paquetes.length; i++) {
+    //         if(paquetes[i].agencia!=req.query.agencia)
+    //             paquetes.splice(i, 1);
+    //     }            
 
-        if(paquetes.length==0)
-            mensaje += "La agencia que ha indicado no existe o no tiene ningún paquete en curso con esa agencia";
-    }
+    //     if(paquetes.length==0)
+    //         mensaje += "La agencia que ha indicado no existe o no tiene ningún paquete en curso con esa agencia";
+    // }
 
 
     if(paquetes.length!=0)
